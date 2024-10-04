bfbe0930d60d58e91a9a588c770a93b190589189
charlie <charlieeoshea@gmail.com>
2024-10-04 14:23:54 +0100

    some lil polish

--- ba3a_prototype/player/player.gd
+++ ba3a_prototype/player/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 3.0
 
 # onready vars
 @onready var mesh: MeshInstance3D = $mesh
+@onready var vfx_foam: GPUParticles3D = $mesh/vfx_foam
 
 func _ready() -> void:
 	RenderingServer.global_shader_parameter_set("enable_world_bend", true)
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
+		velocity.x = direction.x * SPEED 
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
@@ -23,5 +23,9 @@ func _physics_process(delta: float) -> void:
 	var vec_nor = velocity.normalized()
 	if vec_nor.length() > 0.0:
 		mesh.rotation.y = lerp_angle(mesh.rotation.y, Vector2(-vec_nor.x, vec_nor.z).angle(), 2.0 * delta)
+		
+		vfx_foam.emitting = true
+	else:
+		vfx_foam.emitting = false
 	
 	move_and_slide()
