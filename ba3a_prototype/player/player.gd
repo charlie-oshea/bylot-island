796f6235bc5424c3fcc4f8cedf093b17d085d726
charlie <charlieeoshea@gmail.com>
2024-10-04 13:02:06 +0100

    more style expo

--- ba3a_prototype/player/player.gd
+++ ba3a_prototype/player/player.gd
@@ -1,30 +1,27 @@
 extends CharacterBody3D
 
 
-const SPEED = 15.0
+const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+# onready vars
+@onready var mesh: MeshInstance3D = $mesh
 
 func _ready() -> void:
 	RenderingServer.global_shader_parameter_set("enable_world_bend", true)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+	
+	var vec_nor = velocity.normalized()
+	if vec_nor.length() > 0.0:
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, Vector2(-vec_nor.x, vec_nor.z).angle(), 2.0 * delta)
+	
 	move_and_slide()
