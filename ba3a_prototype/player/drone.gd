b746785dc6efed80da5666e47bd875d6c12160e0
charlie <charlieeoshea@gmail.com>
2024-10-09 16:15:18 +0100

    drone movement and photos- fix viewport stuff with images

---
+++ ba3a_prototype/player/drone.gd
@@ -0,0 +1,74 @@
+extends CharacterBody3D
+
+var enabled: bool = false
+var speed := 500.0
+var cam_zoom_speed := 50.0
+
+@onready var camera_rig: Node3D = $drone_camera_rig
+@onready var camera: Camera3D = $drone_camera_rig/drone_camera
+
+func _physics_process(delta: float) -> void:
+	if enabled:
+		handle_camera_zoom(delta)
+		
+		var joy_vector_r := Vector2.ZERO
+		joy_vector_r.x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+		joy_vector_r.y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+		
+		var joy_vector_l := Vector2.ZERO
+		joy_vector_l.x = Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
+		joy_vector_l.y = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
+		
+		var input_velocity := Vector3.ZERO
+		
+		if joy_vector_r.length() > Settings.DEADZONE:
+			# left/right
+			rotation.y -= joy_vector_r.x * Settings.CAM_ROTATE_SENS * delta
+			
+			# up/down
+			camera_rig.rotation.x -= deg_to_rad(joy_vector_r.y) * Settings.CAM_ROTATE_SENS
+			camera_rig.rotation.x = clamp(camera_rig.rotation.x, deg_to_rad(-40), deg_to_rad(40))
+		
+		if joy_vector_l.length() > Settings.DEADZONE:
+			# up/down
+			var forward_dir := -transform.basis.z.normalized()
+			
+			input_velocity += forward_dir * -joy_vector_l.y * speed * delta
+			
+			# left/right
+			var side_dir := transform.basis.x.normalized()
+			
+			input_velocity += side_dir * joy_vector_l.x * speed * delta
+		
+		if Input.is_action_pressed("drone_up") or Input.is_action_pressed("drone_down"):
+			var y_input = Input.get_action_strength("drone_up") - Input.get_action_strength("drone_down")
+			input_velocity.y += y_input * speed * delta
+		
+		velocity.x = move_toward(velocity.x, input_velocity.x, 3.0 * delta)
+		velocity.y = move_toward(velocity.y, input_velocity.y, 3.0 * delta)
+		velocity.z = move_toward(velocity.z, input_velocity.z, 3.0 * delta)
+		
+		
+		move_and_slide()
+		
+		clamp_position()
+
+func clamp_position():
+	global_position.y = clamp(global_position.y, 0.0, 30.0)
+
+func enable():
+	enabled = true
+	
+	camera.current = true
+
+func disable():
+	enabled = false
+	
+	camera.fov = 65.0
+
+func handle_camera_zoom(delta: float) -> void:
+	var cam_zoom := (Input.get_action_strength("camera_zoom_out") - Input.get_action_strength("camera_zoom_in")) * delta
+	camera.fov += cam_zoom * cam_zoom_speed
+	
+	# clamp fov
+	camera.fov = clamp(camera.fov, 20.0, 75.0)
--- ba3a_prototype/player/player_walk.gd
+++ ba3a_prototype/player/player_walk.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 const SPEED = 6.0
 
 # cam vars
-var in_camera: bool = false
+var in_drone: bool = false
 var cam_zoom_speed := 50.0
 var photos_taken := 0
 
@@ -14,19 +14,16 @@ var notebook: Notebook
 # misc vars
 var last_input_dir := Vector2.ZERO
 var last_move_direction := Vector3.ZERO
-var stored_camera_rig_rotation: Vector3
 
 # onready vars
 @onready var mesh: MeshInstance3D = $mesh
 @onready var camera_rig: Node3D = $camera_rig
 @onready var walk_camera: Camera3D = $camera_rig/SpringArm3D/camera
-@onready var photo_camera: Camera3D = $photo_camera_rig/photo_camera
-@onready var photo_camera_rig: Node3D = $photo_camera_rig
-@onready var photo_viewport_camera: Camera3D = $camera_viewport/photo_viewport_camera
-@onready var photo_cam_control: Node3D = $photo_camera_rig/photo_camera/photo_cam_control
-@onready var camera_viewport: SubViewport = $camera_viewport
+
 @onready var ui_parent: Control = $CanvasLayer/ui_parent
 @onready var ui_anims: AnimationPlayer = $CanvasLayer/ui_top/ui_anims
+
+@onready var drone: CharacterBody3D = $drone
 
 
 # preloads
@@ -42,11 +39,8 @@ func _physics_process(delta: float) -> void:
 		handle_camera_rotation(delta)
 		move_and_slide()
 	
-	if in_camera:
-		handle_photo_camera_rotation(delta)
-		handle_photo_camera_zoom(delta)
-	
-	handle_camera_rot_matching()
+	if in_drone:
+		pass
 
 func handle_movement(delta: float) -> void:
 	var input_dir := Vector2.ZERO
@@ -63,7 +57,6 @@ func handle_movement(delta: float) -> void:
 	if input_dir.length() > 1:
 		input_dir = input_dir.normalized()
 		last_input_dir = input_dir
-
 	
 	var direction := (camera_rig.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -93,48 +86,20 @@ func handle_camera_rotation(delta: float) -> void:
 		#var target_angle = atan2(-last_move_direction.x, -last_move_direction.z)
 		#camera_rig.rotation.y = lerp_angle(camera_rig.rotation.y, target_angle, 0.3 * delta)
 
-func handle_photo_camera_rotation(delta: float) -> void:
-	var joy_vector := Vector2.ZERO
-	joy_vector.x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
-	joy_vector.y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-	
-	if joy_vector.length() > Settings.DEADZONE and in_camera:
-		# left/right
-		photo_camera_rig.rotate_y(deg_to_rad(-joy_vector.x) * Settings.CAM_ROTATE_SENS)
-		
-		# up/down
-		photo_camera.rotate_x(deg_to_rad(-joy_vector.y) * Settings.CAM_ROTATE_SENS)
-		photo_camera.rotation.x = clamp(photo_camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-
-func handle_photo_camera_zoom(delta: float) -> void:
-	var cam_zoom := (Input.get_action_strength("camera_zoom_out") - Input.get_action_strength("camera_zoom_in")) * delta
-	photo_viewport_camera.fov += cam_zoom * cam_zoom_speed
-	
-	# clamp fov
-	photo_viewport_camera.fov = clamp(photo_viewport_camera.fov, 20.0, 75.0)
-
-func handle_camera_rot_matching():
-	if in_camera:
-		# make base camera face same direction
-		camera_rig.rotation.y = photo_camera_rig.rotation.y
-	else:
-		# make photo camera face same direction as base camera
-		photo_camera_rig.rotation.y = camera_rig.rotation.y
-
 func can_move() -> bool:
-	if in_camera:
+	if in_drone:
 		return false
 	if in_notebook:
 		return false
 	return true
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("enter_camera") and can_move():
-		if in_camera:
-			camera_transition(false)
+	if event.is_action_pressed("enter_camera"):
+		if in_drone:
+			drone_transition(false)
 		else:
-			camera_transition(true)
-	if event.is_action_pressed("take_picture") and in_camera:
+			drone_transition(true)
+	if event.is_action_pressed("take_picture") and in_drone:
 		take_picture()
 	if event.is_action_pressed("enter_notebook"):
 		if in_notebook:
@@ -145,8 +110,8 @@ func _input(event: InputEvent) -> void:
 			ui_parent.add_child(n)
 			notebook = n
 	if event.is_action_pressed("ui_cancel"):
-		if in_camera:
-			camera_transition(false)
+		if in_drone:
+			drone_transition(false)
 		elif in_notebook:
 			close_notebook()
 
@@ -157,66 +122,21 @@ func close_notebook():
 
 ### camera functions ###
 
-func camera_transition(to_photo: bool):
+func drone_transition(to_photo: bool):
 	if to_photo:
-		in_camera = true
-		
-		stored_camera_rig_rotation = camera_rig.rotation
-		
-		var previous_pos = walk_camera.global_position
-		var previous_rot = walk_camera.rotation
-		
-		var t1 = get_tree().create_tween()
-		t1.tween_property(walk_camera, "global_position", photo_camera.global_position, 0.2).set_ease(Tween.EASE_IN_OUT)
-		var t2 = get_tree().create_tween()
-		t2.tween_property(walk_camera, "rotation", photo_camera.rotation, 0.2).set_ease(Tween.EASE_IN_OUT)
-		await t2.finished
+		in_drone = true
 		
 		# set current cam
-		photo_camera.current = true
-		
-		# show cam
-		photo_cam_control.visible = true
-		
-		# hide mesh
-		mesh.visible = false
-		
-		# reset walk cam
-		walk_camera.global_position = previous_pos
-		walk_camera.rotation = previous_rot
-		
+		drone.enable()
 	else:
-		in_camera = false
+		in_drone = false
 		
-		var previous_pos = photo_camera.global_position
-		var previous_rot = photo_camera.rotation
-		
-		var t1 = get_tree().create_tween()
-		t1.tween_property(photo_camera, "global_position", walk_camera.position, 0.2).set_ease(Tween.EASE_IN_OUT)
-		var t2 = get_tree().create_tween()
-		t2.tween_property(photo_camera, "global_rotation", walk_camera.rotation, 0.2).set_ease(Tween.EASE_IN_OUT)
-		await t2.finished
-		
+		drone.disable()
 		walk_camera.current = true
-		
-		camera_rig.rotation = stored_camera_rig_rotation
-		
-		# reset photo cam
-		photo_camera.global_position = previous_pos
-		photo_camera.rotation = previous_rot
-		
-		# hide cam
-		photo_cam_control.visible = false
-		
-		# show mesh
-		mesh.visible = true
-		
-		# reset camera zoom
-		photo_viewport_camera.fov = 65.0
 
 func take_picture():
 	ui_anims.play("flash")
-	var image = camera_viewport.get_texture().get_image()
+	var image = get_viewport().get_texture().get_image()
 	
 	var photo := Photo.new()
 	photo.id = photos_taken
@@ -228,6 +148,6 @@ func take_picture():
 	ui_parent.add_child(ps)
 	ps.setup(photo)
 	
-	camera_transition(false)
+	drone_transition(false)
 	
 	PhotoManager.album.append(photo)
